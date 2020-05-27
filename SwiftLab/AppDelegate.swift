eb3dcd3af0b2bb57736b3768df6a246f7d01851a
rex.chen <rex.chen@appier.com>
2020-05-27 14:46:18 +0800

    first

--- SwiftLab/AppDelegate.swift
+++ SwiftLab/AppDelegate.swift
@@ -7,14 +7,14 @@
 //
 
 import UIKit
+import AIRexLib
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        QGSdk.getSharedInstance().onStart("ww", setDevProfile: true)
         return true
     }
 
